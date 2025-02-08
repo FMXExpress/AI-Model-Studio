f1808ba07f14d9a66e43a045b546b50ce5fbd66b
lmbelo <lucas.belo@live.com>
2025-02-08 17:44:10 -0300

    Project reference for ExploreCard frame

--- AIModelStudio.dpr
+++ AIModelStudio.dpr
@@ -6,7 +6,8 @@ uses
   uMainForm in 'uMainForm.pas' {MainForm},
   uItemFrame in 'uItemFrame.pas' {FrameItem: TFrame},
   uPredictFrame in 'uPredictFrame.pas' {PredictFrame: TFrame},
-  uReplicate in 'uReplicate.pas';
+  uReplicate in 'uReplicate.pas',
+  uExploreCard in 'uExploreCard.pas' {ExploreCard: TFrame};
 
 {$R *.res}
 
