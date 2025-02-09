b5892b4b960b023dcb6098a59da3419ef498b707
lmbelo <lucas.belo@live.com>
2025-02-09 12:10:38 -0300

    Card options menu to run online/offline and show details

--- uExploreCard.pas
+++ uExploreCard.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   FMX.Controls.Presentation, FMX.Layouts, FMX.Objects, System.Net.URLClient,
-  System.Net.HttpClient, System.Net.HttpClientComponent;
+  System.Net.HttpClient, System.Net.HttpClientComponent, FMX.Menus, uReplicate;
 
 type
   TExploreCard = class(TFrame)
@@ -18,30 +18,32 @@ type
     aniConverImage: TAniIndicator;
     NetHTTPClient1: TNetHTTPClient;
     recCard: TRectangle;
+    PopupMenu1: TPopupMenu;
+    miRunLocally: TMenuItem;
+    miRunOnline: TMenuItem;
+    miModelDetails: TMenuItem;
+    layBar: TLayout;
+    btnOptions: TButton;
     procedure NetHTTPClient1RequestCompleted(const Sender: TObject;
       const AResponse: IHTTPResponse);
     procedure NetHTTPClient1RequestError(const Sender: TObject;
       const AError: string);
     procedure NetHTTPClient1RequestException(const Sender: TObject;
       const AError: Exception);
+    procedure miModelDetailsClick(Sender: TObject);
+    procedure miRunLocallyClick(Sender: TObject);
+    procedure miRunOnlineClick(Sender: TObject);
+    procedure btnOptionsClick(Sender: TObject);
   private
+    FModel: TModel;
     FImage: TStream;
     FBusy: boolean;
-    FModelName: string;
-    FDescription: string;
-    FRunCount: integer;
-    FCoverImageUrl: string;
     function FormatRunCount(): string;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy(); override;
 
-    procedure Init();
-
-    property ModelName: string read FModelName write FModelName;
-    property Description: string read FDescription write FDescription;
-    property RunCount: integer read FRunCount write FRunCount;
-    property CoverImageUrl: string read FCoverImageUrl write FCoverImageUrl;
+    procedure Init(const AModel: TModel);
   end;
 
 implementation
@@ -51,9 +53,26 @@ uses
 
 {$R *.fmx}
 
+procedure TExploreCard.btnOptionsClick(Sender: TObject);
+begin
+  var LParent := Self.Parent;
+  while Assigned(LParent) do
+    if LParent is TCustomForm then
+      Break
+    else
+      LParent := LParent.Parent;
+
+  if not Assigned(LParent) then
+    Exit;
+
+  var LPos := TCustomForm(LParent).ClientToScreen(
+    btnOptions.LocalToAbsolute(PointF(0, btnOptions.Height)));
+  PopupMenu1.Popup(LPos.X, LPos.Y);
+end;
+
 constructor TExploreCard.Create(AOwner: TComponent);
 begin
-  inherited;
+  inherited Create(AOwner);
   FImage := TMemoryStream.Create();
 end;
 
@@ -66,6 +85,7 @@ begin
     end);
 
   FImage.Free();
+  FModel.Free();
   inherited;
 end;
 
@@ -105,25 +125,44 @@ end;
 
 function TExploreCard.FormatRunCount: string;
 begin
-  if RunCount >= 1000000000 then
-    Result := Format('%0.1fB', [RunCount / 1000000000]) // Billion
-  else if RunCount >= 1000000 then
-    Result := Format('%0.1fM', [RunCount / 1000000]) // Million
-  else if RunCount >= 1000 then
-    Result := Format('%0.1fK', [RunCount / 1000]) // Thousand
+  if FModel.RunCount >= 1000000000 then
+    Result := Format('%0.1fB', [FModel.RunCount / 1000000000]) // Billion
+  else if FModel.RunCount >= 1000000 then
+    Result := Format('%0.1fM', [FModel.RunCount / 1000000]) // Million
+  else if FModel.RunCount >= 1000 then
+    Result := Format('%0.1fK', [FModel.RunCount / 1000]) // Thousand
   else
-    Result := IntToStr(RunCount); // Regular number
+    Result := IntToStr(FModel.RunCount); // Regular number
 end;
 
-procedure TExploreCard.Init;
+procedure TExploreCard.Init(const AModel: TModel);
 begin
-  lbName.Text := FModelName;
-  lbDescription.Text := FDescription;
+  Assert(Assigned(AModel), 'Param "AModel" not assigned.');
+
+  FModel := AModel.Clone();
+
+  lbName.Text := FModel.Id;
+  lbDescription.Text := FModel.Description;
   lbRuns.Text := #$1F680 + ' ' + FormatRunCount();
 
   aniConverImage.Enabled := true;
   aniConverImage.Visible := true;
-  NetHttpClient1.Get(FCoverImageUrl);
+  NetHttpClient1.Get(FModel.CoverImageUrl);
+end;
+
+procedure TExploreCard.miModelDetailsClick(Sender: TObject);
+begin
+  //
+end;
+
+procedure TExploreCard.miRunLocallyClick(Sender: TObject);
+begin
+  //
+end;
+
+procedure TExploreCard.miRunOnlineClick(Sender: TObject);
+begin
+  //
 end;
 
 end.
